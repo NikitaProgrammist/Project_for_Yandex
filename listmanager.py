d8602324b461aeb3b895331dd94cbe8b8e55c1bf
NikitaProgrammist <nikita.govorov.nikita@yandex.ru>
2023-11-08 16:26:47 +0300

    typehints

--- listmanager.py
+++ listmanager.py
@@ -5,43 +5,65 @@ from styles_and_delegations import QComboBox
 
 
 class ListManager(QtWidgets.QWidget):
-    def __init__(self, user):
+    def __init__(self, user: str) -> None:
         super().__init__()
         self.user = user + '_list'
+        self.select_row = -1
+
         self.connection = sqlite3.connect('dist/task_manager.db')
         self.cursor = self.connection.cursor()
+
         self.initUI()
         self.filter_func()
 
-    def initUI(self):
+    def initUI(self) -> None:
         main_layout = QtWidgets.QVBoxLayout(self)
-        self.task_table = QtWidgets.QTableView(self)
-        self.task_table.setEditTriggers(QtWidgets.QAbstractItemView.NoEditTriggers)
+
         low_layout = QtWidgets.QHBoxLayout()
+
         self.add_task_button = QtWidgets.QPushButton("Добавить задачу")
+        self.add_task_button.clicked.connect(self.add_task)
+
         self.edit_task_button = QtWidgets.QPushButton("Редактировать задачу")
+        self.edit_task_button.clicked.connect(self.edit_task)
+
         self.delete_task_button = QtWidgets.QPushButton("Удалить задачу")
+        self.delete_task_button.clicked.connect(self.delete_task)
+
         self.show_all_button = QtWidgets.QPushButton("Все задачи")
+        self.show_all_button.clicked.connect(self.show_all_tasks)
+
         low_layout.addWidget(self.add_task_button)
         low_layout.addWidget(self.edit_task_button)
         low_layout.addWidget(self.delete_task_button)
         low_layout.addWidget(self.show_all_button)
+
+        self.task_table = QtWidgets.QTableView(self)
+
         high_layout = QtWidgets.QHBoxLayout()
+
         self.choose_priority = QComboBox(self)
         self.choose_priority.addItem("Все")
         self.priority_items = sorted(set([str(_[0]) for _ in self.cursor.execute(f"""SELECT priority 
-        FROM {self.user}""").fetchall()]), key=int)
+                FROM {self.user}""").fetchall()]), key=int)
         self.choose_priority.addItems(self.priority_items)
-        self.choose_priority.model().itemChanged.connect(self.f)
+        self.choose_priority.model().itemChanged.connect(self.select_priority_items)
+
         self.filter = QtWidgets.QLineEdit(self)
+
         self.filter_button = QtWidgets.QPushButton("Искать")
+        self.filter_button.clicked.connect(self.filter_func)
+
         high_layout.addWidget(self.choose_priority)
         high_layout.addWidget(self.filter)
         high_layout.addWidget(self.filter_button)
+
         main_layout.addLayout(high_layout)
         main_layout.addWidget(self.task_table)
         main_layout.addLayout(low_layout)
+
         self.query = QtSql.QSqlQuery()
+
         self.model = QtSql.QSqlTableModel(self)
         self.model.setTable(self.user)
         self.model.setHeaderData(0, QtCore.Qt.Horizontal, 'ИД')
@@ -51,23 +73,19 @@ class ListManager(QtWidgets.QWidget):
         self.model.setHeaderData(4, QtCore.Qt.Horizontal, 'Помеченная')
         self.model.setHeaderData(5, QtCore.Qt.Horizontal, 'Важная')
         self.model.select()
+
         self.task_table.setModel(self.model)
         self.task_table.setSelectionBehavior(QtWidgets.QAbstractItemView.SelectRows)
         self.task_table.setSelectionMode(QtWidgets.QAbstractItemView.SingleSelection)
         self.task_table.resizeColumnsToContents()
         self.task_table.resizeRowsToContents()
+        self.task_table.setEditTriggers(QtWidgets.QAbstractItemView.NoEditTriggers)
         self.task_table.hideColumn(0)
         self.task_table.hideColumn(4)
         self.task_table.hideColumn(5)
-        self.filter_button.clicked.connect(self.filter_func)
-        self.add_task_button.clicked.connect(self.add_task)
-        self.edit_task_button.clicked.connect(self.edit_task)
-        self.delete_task_button.clicked.connect(self.delete_task)
-        self.show_all_button.clicked.connect(self.show_all_tasks)
         self.task_table.clicked.connect(self.cell_changed)
-        self.row = -1
 
-    def f(self, item):
+    def select_priority_items(self, item: QtCore.QItemSelection) -> None:
         if item == self.choose_priority.model().item(0, 0) and not item.checkState():
             for i in range(1, self.choose_priority.count()):
                 self.choose_priority.model().item(i, 0).setCheckState(QtCore.Qt.Unchecked)
@@ -77,23 +95,25 @@ class ListManager(QtWidgets.QWidget):
                 self.choose_priority.model().item(i, 0).setCheckState(QtCore.Qt.Checked)
             return
 
-    def cell_changed(self, index):
-        if index.row() != self.row:
+    def cell_changed(self, index: QtWidgets.QTableWidgetItem) -> None:
+        if index.row() != self.select_row:
             self.task_table.selectionModel().clearSelection()
             self.task_table.selectRow(index.row())
-            self.row = index.row()
+            self.select_row = index.row()
         else:
-            self.row = -1
+            self.select_row = -1
             self.task_table.selectionModel().clearSelection()
 
-    def filter_func(self):
+    def filter_func(self) -> None:
         self.model.select()
+
         priority = []
         for i in range(self.choose_priority.count()):
             item = self.choose_priority.model().item(i)
             if item.checkState() == QtCore.Qt.Checked:
                 priority.append(item.text())
         name = self.filter.text()
+
         if "Все" not in priority:
             self.query.exec(f"""SELECT * FROM {self.user} WHERE priority in ({', '.join(priority)}) and name like 
             '%{name}%' ORDER BY priority DESC, name ASC""")
@@ -101,13 +121,14 @@ class ListManager(QtWidgets.QWidget):
             self.query.exec(f"""SELECT * FROM {self.user} WHERE name like '%{name}%' 
             ORDER BY priority DESC, name ASC""")
         self.model.setQuery(self.query)
+
         self.task_table.resizeColumnsToContents()
         self.task_table.resizeRowsToContents()
         self.task_table.hideColumn(0)
         self.task_table.hideColumn(4)
         self.task_table.hideColumn(5)
 
-    def show_all_tasks(self):
+    def show_all_tasks(self) -> None:
         self.model.select()
         self.query.exec(f"""SELECT * FROM {self.user}
         ORDER BY priority DESC, name ASC""")
@@ -115,7 +136,7 @@ class ListManager(QtWidgets.QWidget):
         self.task_table.resizeColumnsToContents()
         self.task_table.resizeRowsToContents()
 
-    def add_task(self):
+    def add_task(self) -> None:
         dialog = ProblemDialog(self)
         if dialog.exec_() == QtWidgets.QDialog.Accepted:
             name = dialog.name_field.text()
@@ -126,6 +147,7 @@ class ListManager(QtWidgets.QWidget):
                 self.priority_items.append(str(priority))
             marketed = dialog.marketed_field.currentText()
             importance = dialog.importance_field.currentText()
+
             record = self.model.record()
             record.setValue("name", name)
             record.setValue("description", description)
@@ -135,10 +157,11 @@ class ListManager(QtWidgets.QWidget):
             self.model.insertRecord(-1, record)
         self.filter_func()
 
-    def edit_task(self):
+    def edit_task(self) -> None:
         selected_row = self.task_table.selectionModel().currentIndex().row()
         if selected_row >= 0:
             dialog = ProblemDialog(self)
+
             record = self.model.record(selected_row)
             id = record.value("id")
             name = record.value("name")
@@ -146,23 +169,26 @@ class ListManager(QtWidgets.QWidget):
             priority = record.value("priority")
             marketed = record.value("marketed")
             importance = record.value("importance")
+
             dialog.name_field.setText(name)
             dialog.description_field.setPlainText(description)
             dialog.priority_field.setValue(priority)
             dialog.marketed_field.setCurrentText(marketed)
             dialog.importance_field.setCurrentText(importance)
+
             if dialog.exec_() == QtWidgets.QDialog.Accepted:
                 name = dialog.name_field.text()
                 description = dialog.description_field.toPlainText()
                 priority = dialog.priority_field.value()
                 marketed = dialog.marketed_field.currentText()
                 importance = dialog.importance_field.currentText()
+
                 self.query.exec(f"""REPLACE INTO {self.user} (id, name, description, priority, marketed, importance) 
                 VALUES ({id}, '{name}', '{description}', {priority}, '{marketed}', '{importance}')""")
                 self.model.setQuery(self.query)
         self.filter_func()
 
-    def delete_task(self):
+    def delete_task(self) -> None:
         selected_row = self.task_table.selectionModel().currentIndex().row()
         if selected_row >= 0:
             self.model.removeRow(selected_row)
@@ -170,14 +196,16 @@ class ListManager(QtWidgets.QWidget):
 
 
 class MarkedTasks(ListManager):
-    def filter_func(self):
+    def filter_func(self) -> None:
         self.model.select()
+
         priority = []
         for i in range(self.choose_priority.count()):
             item = self.choose_priority.model().item(i)
             if item.checkState() == QtCore.Qt.Checked:
                 priority.append(item.text())
         name = self.filter.text()
+
         if "Все" not in priority:
             self.query.exec(f"""SELECT * FROM {self.user} WHERE marketed = 'Да' and 
             priority in ({', '.join(priority)}) and name like '%{name}%'
@@ -186,13 +214,14 @@ class MarkedTasks(ListManager):
             self.query.exec(f"""SELECT * FROM {self.user} WHERE marketed = 'Да' and name like '%{name}%'
             ORDER BY priority DESC, name ASC""")
         self.model.setQuery(self.query)
+
         self.task_table.resizeColumnsToContents()
         self.task_table.resizeRowsToContents()
         self.task_table.hideColumn(0)
         self.task_table.hideColumn(4)
         self.task_table.hideColumn(5)
 
-    def show_all_tasks(self):
+    def show_all_tasks(self) -> None:
         self.model.select()
         self.query.exec(f"""SELECT * FROM {self.user} WHERE marketed = 'Да'
         ORDER BY priority DESC, name ASC""")
@@ -202,14 +231,16 @@ class MarkedTasks(ListManager):
 
 
 class ImportantTasks(ListManager):
-    def filter_func(self):
+    def filter_func(self) -> None:
         self.model.select()
+
         priority = []
         for i in range(self.choose_priority.count()):
             item = self.choose_priority.model().item(i)
             if item.checkState() == QtCore.Qt.Checked:
                 priority.append(item.text())
         name = self.filter.text()
+
         if "Все" not in priority:
             self.query.exec(f"""SELECT * FROM {self.user} WHERE importance = 'Да' and 
             priority in ({', '.join(priority)}) and name like '%{name}%'
@@ -218,13 +249,14 @@ class ImportantTasks(ListManager):
             self.query.exec(f"""SELECT * FROM {self.user} WHERE importance = 'Да' and name like '%{name}%'
             ORDER BY priority DESC, name ASC""")
         self.model.setQuery(self.query)
+
         self.task_table.resizeColumnsToContents()
         self.task_table.resizeRowsToContents()
         self.task_table.hideColumn(0)
         self.task_table.hideColumn(4)
         self.task_table.hideColumn(5)
 
-    def show_all_tasks(self):
+    def show_all_tasks(self) -> None:
         self.model.select()
         self.query.exec(f"""SELECT * FROM {self.user} WHERE importance = 'Да'
         ORDER BY priority DESC, name ASC""")
--- styles_and_delegations.py
+++ styles_and_delegations.py
@@ -1,13 +1,15 @@
+from typing import Any
+
 from PyQt5 import QtWidgets, QtCore, QtGui
 
 
 class DateDelegate(QtWidgets.QStyledItemDelegate):
-    def displayText(self, value, locale):
+    def displayText(self, value: str, locale: QtCore.QLocale) -> str:
         return ':'.join(value.split('-')[::-1])
 
 
 class QCalendarWidget(QtWidgets.QCalendarWidget):
-    def __init__(self, parent=None):
+    def __init__(self, parent: QtWidgets.QWidget = None) -> None:
         super().__init__(parent)
         self.setStyleSheet("background-color: #FFFFFF; color: #FF0000;")
         font = QtGui.QFont("Roboto", 10)
@@ -17,12 +19,12 @@ class QCalendarWidget(QtWidgets.QCalendarWidget):
 
 
 class QComboBox(QtWidgets.QComboBox):
-    def addItem(self, item):
-        super(QComboBox, self).addItem(item)
+    def addItem(self, text: str, userData: Any | None = None) -> None:
+        super(QComboBox, self).addItem(text)
         item = self.model().item(self.count()-1, 0)
         item.setFlags(QtCore.Qt.ItemIsUserCheckable | QtCore.Qt.ItemIsEnabled)
         item.setCheckState(QtCore.Qt.Checked)
 
-    def addItems(self, items):
+    def addItems(self, items: Any) -> None:
         for i in items:
             self.addItem(i)
